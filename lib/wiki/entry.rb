a37f9561879cbed5c8a74c783d8df5f9fed53c9e
Benjamin Willems <bw@benjaminwil.info>
2018-12-11 16:33:03 -0800

    Add an undefined message for entries without frontmatter

--- lib/wiki/entry.rb
+++ lib/wiki/entry.rb
@@ -3,18 +3,23 @@ require 'front_matter_parser'
 module Wiki
   class Entry
     attr_reader :file, :title, :description, :sort_order, :is_readme
-    
+
     def initialize(filename)
       @file = filename
-      @title = metadata['title']
-      @description = metadata['description']
+      @title = metadata['title'] || frontmatter_undefined
+      @description = metadata['description'] || frontmatter_undefined
     end
-    
+
     private
 
     def metadata
       file = ::FrontMatterParser::Parser.parse_file(@file)
       file.front_matter
+    end
+
+    def frontmatter_undefined
+      "Frontmatter undefined for " \
+      "#{filename - ::Wiki::Configuration.project_root}"
     end
 
   end
