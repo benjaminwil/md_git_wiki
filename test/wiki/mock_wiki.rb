47d413b8d1cd4299996e054978900927613ef6df
Benjamin Willems <bw@benjaminwil.info>
2019-01-04 08:47:47 -0800

    wip: improve mockwiki module methods

--- test/wiki/mock_wiki.rb
+++ test/wiki/mock_wiki.rb
@@ -1,61 +1,36 @@
-require 'random-word'
-require 'stringio'
 require 'tmpdir'
 
 module Wiki
   module MockWiki
 
-    def wiki_initialize
-      @project_root = Dir.mktmpdir("project_root")
-      nouns = 10.times.map { "#{RandomWord.nouns.next}" }
-      nouns.each { |noun| new_subdirectory(noun) }
-      @directory_1 = wiki_directories[1]
-      @directory_2 = wiki_directories[-1]
-      @entry_1 = wiki_entries[0]
-      @entry_2 = wiki_entries[-1]
+    def create_mock_wiki
+      require 'pry'
+      binding.pry
+      @wiki = Dir.mktmpdir('wiki')
+      contents = File.open(File.join File.expand_path("#{__FILE__}/../../mock_wiki"), '/.')
+      FileUtils.copy_entry(contents, @wiki)
+      generate_entries
+      return @wiki
     end
 
-    def wiki_destroy
-      FileUtils.remove_entry(@project_root)
+    def destroy_mock_wiki
+      FileUtils.remove_entry(@wiki)
     end
 
-    def wiki_directories
-      Dir.glob("#{@project_root}/*")
+    def directories
+      Dir.glob("#{@wiki}/*").select { |dir| File.directory?(dir) }
     end
 
-    def wiki_entries
-      Dir.glob("#{@project_root}/**/**/*.md")
+    def entries
+      Dir.glob("#{@wiki}/**/**/*.md")
     end
 
-    def new_subdirectory(subdir)
-      path = File.join("#{@project_root}", subdir)
-      Dir.mkdir(path)
-      new_file(path, "README.md")
-      10.times { new_file(path) }
+    def readmes
+      Dir.glob("#{@wiki}/**/**/README.md")
     end
 
-    def new_file(dir_name, filename="#{random_filename}")
-      path = File.join(dir_name, filename)
-      File.open(path, 'w') do |f|
-        f.write file_contents filename
-      end
-    end
-
-    def random_filename
-      "#{RandomWord.adjs.next}-#{RandomWord.nouns.next}.md"
-    end
-
-    def titleize(random_filename)
-      random_filename.gsub('-', ' ').gsub('.md', '')
-    end
-
-    def file_contents(filename)
-      "--- \n" \
-      "title: #{titleize filename} \n" \
-      "description: Just a short description. \n" \
-      "--- \n\n" \
-      "# #{titleize filename} \n\n" \
-      "Just a single sentence of content for now."
+    def generate_entries
+      entries.each { |e| ::Wiki::Entry.new(e) }
     end
 
   end
